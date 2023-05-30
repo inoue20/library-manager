7828d8f07f9cb61fa32301391510052eaa06de32
inoue <Inoue.Sosuke@technopro.com>
2023-05-30 17:29:50 +0900

    library

--- controller/LibraryController.java
+++ controller/LibraryController.java
@@ -7,20 +7,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.entity.Library;
+import com.example.entity.Log;
 import com.example.service.LibraryService;
+import com.example.service.LogService;
 
 @Controller
 @RequestMapping("library")
 public class LibraryController {
 
 	private final LibraryService libraryService;
+	private final LogService logService;
 
 
 	@Autowired
-    public LibraryController(LibraryService libraryService) {
+    public LibraryController(LibraryService libraryService, LogService logService) {
         this.libraryService = libraryService;
+        this.logService = logService;
     }
 
     @GetMapping
@@ -28,5 +33,12 @@ public class LibraryController {
     	List<Library> libraries = this.libraryService.findAll();
         model.addAttribute("libraries", libraries);
         return "library/index";
+    }
+
+    @GetMapping("borrow")
+    public String borrowingForm(@RequestParam("id") Integer id, Model model) {
+    	List<Log> library = this.logService.findById(id);
+    	model.addAttribute("library", library);
+    	return "library/borrowingForm";
     }
 }
